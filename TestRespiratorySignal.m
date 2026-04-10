2dc3eac72c29b6ddcedad5bbdb657bc11e4c6647
timgervois <tim.gervois11@gmail.com>
2026-04-10 15:28:48 +0200

    ...

--- TestRespiratorySignal.m
+++ TestRespiratorySignal.m
@@ -48,7 +48,7 @@ DataFromStateBlind = PrepareBmObjForCnnPrediction(StateBlindSniffing.bmObj,state
 % Now use the Data.mat file saved from "PrepareBmObjForCnnPrediction" to
 % perform cycle prediction.
 
-disp("Now open Jupyter Notebook and Run 'RespirationBasedStatePrediction'"
+disp("Now open Jupyter Notebook and Run 'RespirationBasedStatePrediction'")
 keyboard
 %% 6) Examine the Predition from the CNN.
 load  CNN_Prediction
